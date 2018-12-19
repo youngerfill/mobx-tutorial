5b93270df6e0c5049980657ef4ec0cfbfed83abd
bergoid <bergoid@users.noreply.github.com>
2018-12-19 16:44:17 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//import ReactDOM from 'react-dom';
+//import * as h from 'react-hyperscript-helpers';
+
+import { observable, computed, mobx } from "mobx"
+
+class ObservableTodoStore {
+    @observable todos = [];
+    @observable pendingRequests = 0;
+
+    constructor() {
+        mobx.autorun(() => console.log(this.report));
+    }
+
+    @computed get completedTodosCount() {
+        return this.todos.filter(
+            todo => todo.completed === true
+        ).length;
+    }
+
+    @computed get report() {
+        if (this.todos.length === 0)
+            return "<none>";
+        return `Next todo: "${this.todos[0].task}". ` +
+            `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+    }
+
+    addTodo(task) {
+        this.todos.push({
+            task: task,
+            completed: false,
+            assignee: null
+        });
+    }
+}
+
+
+const observableTodoStore = new ObservableTodoStore();
+
+
+//const todoStore = new TodoStore();
+
+observableTodoStore.addTodo("read MobX tutorial");
+
+observableTodoStore.addTodo("try MobX");
+
+observableTodoStore.todos[0].completed = true;
+
+observableTodoStore.todos[1].task = "try MobX in own project";
+
+observableTodoStore.todos[0].task = "grok MobX tutorial";
